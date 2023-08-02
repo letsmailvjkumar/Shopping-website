c21aba211a76bdcc467511031158c95e5ca936a5
letsmailvjkumar <letsmailvjkumar@gmail.com>
2023-08-02 19:47:37 +0530

    shop-web-app

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ const btn1 = document.getElementById("signup");
 btn1.addEventListener('click', ()=>{
     window.open("./signup.html");
 })
+window.addEventListener('load', function() {
+    var navbar = document.querySelector('#navbarNavAltMarkup');
+    var bsCollapse = new bootstrap.Collapse(navbar, {toggle: false});
+    bsCollapse.hide();
+});
